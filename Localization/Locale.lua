bf59cda66c9d8ddf89528dc22f7a2a4f56418449
DengSir <DengSir@users.noreply.github.com>
2025-07-22 00:14:53 +0000

    chore: Update libs/locale/data files

--- Localization/Locale.lua
+++ Localization/Locale.lua
@@ -918,160 +918,85 @@ end)
 T('ruRU', function(L)
 -- @locale:language=ruRU@
 L = L or {}
---[[Translation missing --]]
---[[ L["Add advance rule"] = "Add advance rule"--]] 
---[[Translation missing --]]
---[[ L["Add rule"] = "Add rule"--]] 
---[[Translation missing --]]
---[[ L["Advancee rules use ItemSearchModify-1.3"] = "Advancee rules use ItemSearchModify-1.3"--]] 
---[[Translation missing --]]
---[[ L["Already exists"] = "Already exists"--]] 
---[[Translation missing --]]
---[[ L["Are you sure |cffff191919DELETE|r rule and its |cffff1919SUBRULES|r?"] = "Are you sure |cffff191919DELETE|r rule and its |cffff1919SUBRULES|r?"--]] 
---[[Translation missing --]]
---[[ L["Are you sure |cffff191919DELETE|r rule?"] = "Are you sure |cffff191919DELETE|r rule?"--]] 
---[[Translation missing --]]
---[[ L["Are you sure to |cffff1919RESET|r rules?"] = "Are you sure to |cffff1919RESET|r rules?"--]] 
---[[Translation missing --]]
---[[ L["Are you sure you want to restore the current Settings?"] = "Are you sure you want to restore the current Settings?"--]] 
---[[Translation missing --]]
---[[ L["Bag"] = "Bag"--]] 
---[[Translation missing --]]
---[[ L["Bank"] = "Bank"--]] 
---[[Translation missing --]]
---[[ L["Bank and bag stacking together"] = "Bank and bag stacking together"--]] 
---[[Translation missing --]]
---[[ L["Buttons"] = "Buttons"--]] 
---[[Translation missing --]]
---[[ L["Character Specific Settings"] = "Character Specific Settings"--]] 
---[[Translation missing --]]
---[[ L["COMMENT_CLASS"] = "Class items"--]] 
---[[Translation missing --]]
---[[ L["Drag to modify the sorting order"] = "Drag to modify the sorting order"--]] 
---[[Translation missing --]]
---[[ L["Edit rule"] = "Edit rule"--]] 
---[[Translation missing --]]
---[[ L["Enable chat message"] = "Enable chat message"--]] 
---[[Translation missing --]]
---[[ L["Enjoy!"] = "Enjoy!"--]] 
---[[Translation missing --]]
---[[ L["Help"] = "Help"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Cloth"] = "Cloth"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Cooking"] = "Cooking"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Elemental"] = "Elemental"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Enchanting"] = "Enchanting"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Herb"] = "Herb"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Jewelry"] = "Jewelry"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Leather"] = "Leather"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Metal & Stone"] = "Metal & Stone"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Mount"] = "Mount"--]] 
---[[Translation missing --]]
---[[ L["ITEM_TAG: Pet"] = "Pet"--]] 
---[[Translation missing --]]
---[[ L["Keep bank items stack full"] = "Keep bank items stack full"--]] 
---[[Translation missing --]]
---[[ L["KEYWORD_CLASS"] = "Classes"--]] 
---[[Translation missing --]]
---[[ L["Leave bank, pack cancel."] = "Leave bank, pack cancel."--]] 
---[[Translation missing --]]
---[[ L["Left Click"] = "Left Click"--]] 
---[[Translation missing --]]
---[[ L["Name (Optional)"] = "Name (Optional)"--]] 
---[[Translation missing --]]
---[[ L["None"] = "None"--]] 
---[[Translation missing --]]
---[[ L["OPEN_OPTIONS"] = "Open options"--]] 
---[[Translation missing --]]
---[[ L["OPEN_RULE_OPTIONS"] = "Open rule options"--]] 
---[[Translation missing --]]
---[[ L["Pack canceled."] = "Pack canceled."--]] 
---[[Translation missing --]]
---[[ L["Pack finish."] = "Pack finish."--]] 
---[[Translation missing --]]
---[[ L["Player enter combat, pack cancel."] = "Player enter combat, pack cancel."--]] 
---[[Translation missing --]]
---[[ L["Player in combat"] = "Player in combat"--]] 
---[[Translation missing --]]
---[[ L["Player is dead"] = "Player is dead"--]] 
---[[Translation missing --]]
---[[ L["Please drop the item, money or skills."] = "Please drop the item, money or skills."--]] 
---[[Translation missing --]]
---[[ L["Profile"] = "Profile"--]] 
---[[Translation missing --]]
---[[ L["Put in an item to add simple rule"] = "Put in an item to add simple rule"--]] 
---[[Translation missing --]]
---[[ L["Put where?"] = "Put where?"--]] 
---[[Translation missing --]]
---[[ L["Reset rule"] = "Reset rule"--]] 
---[[Translation missing --]]
---[[ L["Restore default Settings"] = "Restore default Settings"--]] 
---[[Translation missing --]]
---[[ L["Reverse pack"] = "Reverse pack"--]] 
---[[Translation missing --]]
---[[ L["Right Click"] = "Right Click"--]] 
---[[Translation missing --]]
---[[ L["Root"] = "Root"--]] 
---[[Translation missing --]]
---[[ L["Rule"] = "Rule"--]] 
---[[Translation missing --]]
---[[ L["Rules"] = "Rules"--]] 
---[[Translation missing --]]
---[[ L["Rules restore to default."] = "Rules restore to default."--]] 
---[[Translation missing --]]
---[[ L["SAVE"] = "Save to bank"--]] 
---[[Translation missing --]]
---[[ L["Save to bank when default packing"] = "Save to bank when default packing"--]] 
---[[Translation missing --]]
---[[ L["SAVING_DESC"] = "Save items to bank."--]] 
---[[Translation missing --]]
---[[ L["SAVING_NAME"] = "Saving"--]] 
---[[Translation missing --]]
---[[ L["Select an icon (Optional)"] = "Select an icon (Optional)"--]] 
---[[Translation missing --]]
---[[ L["Some slot is locked"] = "Some slot is locked"--]] 
---[[Translation missing --]]
---[[ L["SORT"] = "Default pack"--]] 
---[[Translation missing --]]
---[[ L["SORT_ASC"] = "Sort asc"--]] 
---[[Translation missing --]]
---[[ L["SORT_BAG"] = "Sort bag"--]] 
---[[Translation missing --]]
---[[ L["SORT_BAG_ASC"] = "Sort bag asc"--]] 
---[[Translation missing --]]
---[[ L["SORT_BAG_DESC"] = "Sort bag desc"--]] 
---[[Translation missing --]]
---[[ L["SORT_BANK"] = "Sort bank"--]] 
---[[Translation missing --]]
---[[ L["SORT_BANK_ASC"] = "Sort bank asc"--]] 
---[[Translation missing --]]
---[[ L["SORT_BANK_DESC"] = "Sort bank desc"--]] 
---[[Translation missing --]]
---[[ L["SORT_DESC"] = "Sort desc"--]] 
---[[Translation missing --]]
---[[ L["SORTING_DESC"] = "Sort bags and bank."--]] 
---[[Translation missing --]]
---[[ L["SORTING_NAME"] = "Sorting"--]] 
---[[Translation missing --]]
---[[ L["Tools"] = "Tools"--]] 
---[[Translation missing --]]
---[[ L["Transporter"] = "Transporter"--]] 
---[[Translation missing --]]
---[[ L["UPDATE_RULES_CONFIRM"] = [=[The current version of tdPack2 updates a large number of rules. Do you want to reset the rules to the default settings?
-Help to choose:
-1. I know what this prompt is saying: choose for yourself
-2. I don’t know how to choose: Accept
-]=]--]] 
---[[Translation missing --]]
---[[ L["Usuable"] = "Usuable"--]]
+L["Add advance rule"] = "Добавить расширенное правило"
+L["Add rule"] = "Добавить правило"
+L["Advancee rules use ItemSearchModify-1.3"] = "Расширенные правила используют ItemSearchModify-1.3"
+L["Already exists"] = "Уже существует"
+L["Are you sure |cffff191919DELETE|r rule and its |cffff1919SUBRULES|r?"] = "Вы уверены, что хотите |cffff191919УДАЛИТЬ|r правило и его |cffff1919ПОДПРАВИЛА|r?"
+L["Are you sure |cffff191919DELETE|r rule?"] = "Вы уверены, что хотите |cffff191919УДАЛИТЬ|r правило?"
+L["Are you sure to |cffff1919RESET|r rules?"] = "Вы уверены, что хотите |cffff1919СБРОСИТЬ|r правила?"
+L["Are you sure you want to restore the current Settings?"] = "Вы уверены, что хотите восстановить текущие настройки?"
+L["Bag"] = "Сумка"
+L["Bank"] = "Банк"
+L["Bank and bag stacking together"] = "Сумка и банк складываются вместе"
+L["Buttons"] = "Кнопки"
+L["Character Specific Settings"] = "Настройки для конкретного персонажа"
+L["COMMENT_CLASS"] = "Классовые предметы"
+L["Drag to modify the sorting order"] = "Перетащите, чтобы изменить порядок сортировки"
+L["Edit rule"] = "Редактировать правило"
+L["Enable chat message"] = "Включить сообщения в чате"
+L["Enjoy!"] = "Наслаждайтесь!"
+L["Help"] = "Помощь"
+L["ITEM_TAG: Cloth"] = "Ткань"
+L["ITEM_TAG: Cooking"] = "Кулинария"
+L["ITEM_TAG: Elemental"] = "Стихии"
+L["ITEM_TAG: Enchanting"] = "Зачарование"
+L["ITEM_TAG: Herb"] = "Трава"
+L["ITEM_TAG: Jewelry"] = "Ювелирные изделия"
+L["ITEM_TAG: Leather"] = "Кожа"
+L["ITEM_TAG: Metal & Stone"] = "Металл и камень"
+L["ITEM_TAG: Mount"] = "Маунт"
+L["ITEM_TAG: Pet"] = "Питомец"
+L["Keep bank items stack full"] = "Держать стопки предметов в банке полными"
+L["KEYWORD_CLASS"] = "Классы"
+L["Leave bank, pack cancel."] = "Покинуть банк, упаковка отменена."
+L["Left Click"] = "ЛКМ"
+L["Name (Optional)"] = "Имя (опционально)"
+L["None"] = "Ничего"
+L["OPEN_OPTIONS"] = "Открыть настройки"
+L["OPEN_RULE_OPTIONS"] = "Открыть настройки правил"
+L["Pack canceled."] = "Упаковка отменена."
+L["Pack finish."] = "Упаковка завершена."
+L["Player enter combat, pack cancel."] = "Игрок вступил в бой, упаковка отменена."
+L["Player in combat"] = "Игрок в бою"
+L["Player is dead"] = "Игрок мертв"
+L["Please drop the item, money or skills."] = "Пожалуйста, выбросите предмет, деньги или навыки."
+L["Profile"] = "Профиль"
+L["Put in an item to add simple rule"] = "Поместите предмет, чтобы добавить простое правило"
+L["Put where?"] = "Куда положить?"
+L["Reset rule"] = "Сбросить правило"
+L["Restore default Settings"] = "Восстановить настройки по умолчанию"
+L["Reverse pack"] = "Обратная упаковка"
+L["Right Click"] = "ПКМ"
+L["Root"] = "Корень"
+L["Rule"] = "Правило"
+L["Rules"] = "Правила"
+L["Rules restore to default."] = "Правила восстановлены по умолчанию."
+L["SAVE"] = "Сохранить в банк"
+L["Save to bank when default packing"] = "Сохранять в банк при стандартной упаковке"
+L["SAVING_DESC"] = "Сохранять предметы в банк."
+L["SAVING_NAME"] = "Сохранение"
+L["Select an icon (Optional)"] = "Выберите иконку (опционально)"
+L["Some slot is locked"] = "Некоторые слоты заблокированы"
+L["SORT"] = "Упаковка по умолчанию"
+L["SORT_ASC"] = "Сортировать по возрастанию"
+L["SORT_BAG"] = "Сортировать сумку"
+L["SORT_BAG_ASC"] = "Сортировать сумку по возрастанию"
+L["SORT_BAG_DESC"] = "Сортировать сумку по убыванию"
+L["SORT_BANK"] = "Сортировать банк"
+L["SORT_BANK_ASC"] = "Сортировать банк по возрастанию"
+L["SORT_BANK_DESC"] = "Сортировать банк по убыванию"
+L["SORT_DESC"] = "Сортировать по убыванию"
+L["SORTING_DESC"] = "Сортировать сумки и банк."
+L["SORTING_NAME"] = "Сортировка"
+L["Tools"] = "Инструменты"
+L["Transporter"] = "Транспортер"
+L["UPDATE_RULES_CONFIRM"] = [=[Текущая версия tdPack2 обновляет большое количество правил. Хотите сбросить правила на настройки по умолчанию?
+Помощь в выборе:
+1. Я знаю, о чем этот запрос: выберите самостоятельно
+2. Я не знаю, как выбрать: Принять
+]=]
+L["Usuable"] = "Используемый"
 -- @end-locale@
 end)
 
